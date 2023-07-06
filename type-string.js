fc7045f195effc8774f88c1275ea91c937caec68
Alan Garin <garinsabino@gmail.com>
2023-07-06 01:09:59 -0300

    Adicionei os arquivos de testes já feito.

---
+++ type-string.js
@@ -0,0 +1,12 @@
+const texto1 = 'Olá, mundo!'
+const texto2 = "Olá, mundo!"
+const senha = 'senhaSupersegura456!'
+const stringDeNumeros = '345670';
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Alan'
+
+//concatenação (+) ou `${}`
+console.log(citacao + meuNome);
+
+//template string OU template literal 
